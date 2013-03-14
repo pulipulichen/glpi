eb268aac01d3c5a1d223bf5f12b14e3f172b1fde
billxu <billxu0521@gmail.com>
2013-03-14 15:19:20 +0800

    中文化更新
    外掛中文化

---
+++ plugins/projet/locales/zh_TW.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * @version $Id: HEADER 15930 2012-03-08 15:47:55Z tsmr $
+ -------------------------------------------------------------------------
+ Projet plugin for GLPI
+ Copyright (C) 2003-2012 by the Projet Development Team.
+
+ https://forge.indepnet.net/projects/projet
+ -------------------------------------------------------------------------
+
+ LICENSE
+		
+ This file is part of Projet.
+
+ Projet is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Projet is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Projet. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+$title = "專案";
+
+$LANG['plugin_projet']['title'][1] = "".$title."";
+$LANG['plugin_projet']['title'][2] = "參與者";
+$LANG['plugin_projet']['title'][3] = "任務";
+$LANG['plugin_projet']['title'][5] = "甘特圖";
+
+$LANG['plugin_projet']['profile'][0] = "著作權管理";
+$LANG['plugin_projet']['profile'][1] = "$title";
+
+$LANG['plugin_projet'][0] = "名稱";
+$LANG['plugin_projet'][2] = "註釋";
+$LANG['plugin_projet'][5] = "相關參與者";
+$LANG['plugin_projet'][6] = "已完成";
+$LANG['plugin_projet'][9] = "負責";
+$LANG['plugin_projet'][10] = "敘述";
+$LANG['plugin_projet'][11] = "增加任務";
+$LANG['plugin_projet'][12] = "處理中的任務";
+$LANG['plugin_projet'][13] = "建立新專案";
+$LANG['plugin_projet'][14] = "專案敘述";
+$LANG['plugin_projet'][15] = "Duplicate";
+$LANG['plugin_projet'][17] = "元件";
+$LANG['plugin_projet'][18] = "結果";
+$LANG['plugin_projet'][19] = "狀態";
+$LANG['plugin_projet'][20] = "Processing";
+$LANG['plugin_projet'][22] = "任務標題";
+$LANG['plugin_projet'][23] = "型態";
+$LANG['plugin_projet'][24] = "計畫化";
+$LANG['plugin_projet'][26] = "Planned";
+$LANG['plugin_projet'][29] = "專案名稱";
+$LANG['plugin_projet'][30] = "任務";
+$LANG['plugin_projet'][39] = "其他參與者";
+$LANG['plugin_projet'][40] = "受影響者";
+$LANG['plugin_projet'][41] = "優先度";
+$LANG['plugin_projet'][44] = "母專案";
+$LANG['plugin_projet'][45] = "子專案";
+$LANG['plugin_projet'][46] = "子任務";
+$LANG['plugin_projet'][47] = "進度";
+$LANG['plugin_projet'][48] = "%";
+$LANG['plugin_projet'][49] = "Abandoned";
+$LANG['plugin_projet'][50] = "Associated project(s)";
+$LANG['plugin_projet'][52] = "Project(s) where he is the FK_users";
+$LANG['plugin_projet'][53] = "On standby";
+$LANG['plugin_projet'][55] = "是否與子任務相關";
+$LANG['plugin_projet'][56] = "Depends on the tasks children";
+$LANG['plugin_projet'][57] = "Filed";
+$LANG['plugin_projet'][58] = "母任務";
+$LANG['plugin_projet'][64] = "顯示甘特圖";
+$LANG['plugin_projet'][66] = "Finished task";
+$LANG['plugin_projet'][67] = "Used for planning";
+$LANG['plugin_projet'][68] = "任務列表";
+$LANG['plugin_projet'][69] = "Associated Items(s)";
+$LANG['plugin_projet'][70] = "顯示於全域甘特圖";
+$LANG['plugin_projet'][71] = "Associated color";
+$LANG['plugin_projet'][72] = "花費時間";
+$LANG['plugin_projet'][74] = "全部";
+$LANG['plugin_projet'][75] = "估計需要時間";
+$LANG['plugin_projet'][76] = "Total of effective duration of project tasks";
+$LANG['plugin_projet'][77] = "Total of duration of linked tickets for project";
+
+$LANG['plugin_projet']['setup'][1] = "任務類型";
+$LANG['plugin_projet']['setup'][17] = "Associate";
+$LANG['plugin_projet']['setup'][18] = "Dissociate";
+$LANG['plugin_projet']['setup'][22] = "Project state";
+$LANG['plugin_projet']['setup'][23] = "Task state";
+$LANG['plugin_projet']['setup'][25] = "Associate to project";
+
+$LANG['plugin_projet']['mailing'][0] = "With creation, modification, deletion of a project";
+$LANG['plugin_projet']['mailing'][1] = "With creation, modification, deletion, expiration of a task";
+$LANG['plugin_projet']['mailing'][2] = "User responsible of the project";
+$LANG['plugin_projet']['mailing'][3] = "User responsible of the task";
+$LANG['plugin_projet']['mailing'][4] = "A project has been added";
+$LANG['plugin_projet']['mailing'][5] = "A project has been modified or opened";
+$LANG['plugin_projet']['mailing'][6] = "A project has been deleted";
+$LANG['plugin_projet']['mailing'][7] = "A task has been added";
+$LANG['plugin_projet']['mailing'][8] = "A task has been modified or opened";
+$LANG['plugin_projet']['mailing'][9] = "A task has been deleted";
+$LANG['plugin_projet']['mailing'][10] = "A task is outdated";
+$LANG['plugin_projet']['mailing'][11] = "Group responsible of the task";
+$LANG['plugin_projet']['mailing'][12] = "Supplier responsible of the task";
+$LANG['plugin_projet']['mailing'][13] = "Groupe responsible of the project";
+$LANG['plugin_projet']['mailing'][14] = "Modifications";
+$LANG['plugin_projet']['mailing'][15] = "Outdated tasks";
+$LANG['plugin_projet']['mailing'][16] = "Associated tasks";
+
+?>
